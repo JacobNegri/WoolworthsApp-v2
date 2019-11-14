daa2b35d83167583d27bc79b4da132467a12704d
jacobnegri <jnegr3@gmail.com>
2019-11-14 16:37:06 +1000

    still need to fix the issue of crashing from tile clicks

--- app/src/main/java/com/example/woolworthsapp/SecondActivity.java
+++ app/src/main/java/com/example/woolworthsapp/SecondActivity.java
@@ -7,26 +7,27 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
-public class SecondActivity extends AppCompatActivity implements View.OnClickListener {
+public class SecondActivity extends AppCompatActivity implements View.OnClickListener{
 
-    private CardView contactcard, floralcard, profitlosscard, schedulecard, supplycard;
+    private CardView profitlosscard, supplycard, floralcard, schedulecard, contactcard;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
 
-        contactcard = (CardView) findViewById(R.id.contacts_card);
-        floralcard = (CardView) findViewById(R.id.floral_card);
-        profitlosscard = (CardView) findViewById(R.id.profitloss_card);
-        schedulecard = (CardView) findViewById(R.id.planner_card);
-        supplycard = (CardView) findViewById(R.id.marketupdate_card);
+        profitlosscard = (CardView)findViewById(R.id.profitloss_card);
+        supplycard = (CardView)findViewById(R.id.marketupdate_card);
+        floralcard = (CardView)findViewById(R.id.floral_card);
+        schedulecard = (CardView)findViewById(R.id.planner_card);
+        contactcard = (CardView)findViewById(R.id.contacts_card);
 
-        contactcard.setOnClickListener(this);
-        floralcard.setOnClickListener(this);
         profitlosscard.setOnClickListener(this);
-        schedulecard.setOnClickListener(this);
         supplycard.setOnClickListener(this);
+        floralcard.setOnClickListener(this);
+        schedulecard.setOnClickListener(this);
+        contactcard.setOnClickListener(this);
+
     }
 
     @Override
@@ -34,11 +35,11 @@ public class SecondActivity extends AppCompatActivity implements View.OnClickLis
         Intent i ;
 
         switch (v.getId()) {
-            case R.id.contacts_card : i = new Intent(this,ContactsActivity.class);startActivity(i); break;
-            case R.id.floral_card : i = new Intent(this,FloralActivity.class);startActivity(i); break;
             case R.id.profitloss_card : i = new Intent(this,ProfitLossActivity.class);startActivity(i); break;
-            case R.id.planner_card : i = new Intent(this,ScheduleActivity.class);startActivity(i); break;
             case R.id.marketupdate_card : i = new Intent(this,SupplyActivity.class);startActivity(i); break;
+            case R.id.floral_card : i = new Intent(this,FloralActivity.class);startActivity(i); break;
+            case R.id.planner_card : i = new Intent(this,ScheduleActivity.class);startActivity(i); break;
+            case R.id.contacts_card : i = new Intent(this,ContactsActivity.class);startActivity(i); break;
             default:break;
         }
     }
